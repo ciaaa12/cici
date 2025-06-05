751c7cc70358131762e250140001754922e99013
ciaaa12 <outsuskiciaa@gmail.com>
2025-06-06 00:49:57 +0700

    Add files via upload

--- main.sh
+++ main.sh
@@ -201,29 +201,34 @@ fi
 function base_package() {
 clear
 print_install "Menginstall Packet Yang Dibutuhkan"
-apt install zip pwgen openssl netcat socat cron bash-completion -y
+apt install at -y
+apt install zip pwgen openssl socat cron bash-completion -y
 apt install figlet -y
-apt update -y
-apt upgrade -y
 apt dist-upgrade -y
-systemctl enable chronyd
-systemctl restart chronyd
-systemctl enable chrony
-systemctl restart chrony
-chronyc sourcestats -v
-chronyc tracking -v
 apt install ntpdate -y
 ntpdate pool.ntp.org
-apt install sudo -y
 sudo apt-get clean all
 sudo apt-get autoremove -y
-sudo apt-get install -y debconf-utils
+sudo apt-get install -y debconf-utils util-linux bsdmainutils
 sudo apt-get remove --purge exim4 -y
-sudo apt-get remove --purge ufw firewalld -y
+sudo apt-get remove --purge ufw firewalld apache2 -y
 sudo apt-get install -y --no-install-recommends software-properties-common
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
-sudo apt-get install -y speedtest-cli vnstat libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev bc rsyslog dos2unix zlib1g-dev libssl-dev libsqlite3-dev sed dirmngr libxml-parser-perl build-essential gcc g++ python htop lsof tar wget curl ruby zip unzip p7zip-full python3-pip libc6 util-linux build-essential msmtp-mta ca-certificates bsd-mailx iptables iptables-persistent netfilter-persistent net-tools openssl ca-certificates gnupg gnupg2 ca-certificates lsb-release gcc shc make cmake git screen socat xz-utils apt-transport-https gnupg1 dnsutils cron bash-completion ntpdate chrony jq openvpn easy-rsa
+apt-get -y install \
+  gawk iptables iptables-persistent netfilter-persistent figlet ruby libxml-parser-perl \
+  squid nmap screen curl jq bzip2 gzip coreutils rsyslog iftop htop zip unzip net-tools \
+  sed gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl \
+  neofetch lsof openssl openvpn easy-rsa fail2ban tmux socat cron bash-completion \
+  ntpdate xz-utils gnupg2 dnsutils lsb-release chrony libnss3-dev libnspr4-dev pkg-config libpam0g-dev \
+  libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-openssl-dev flex bison make \
+  libnss3-tools libevent-dev xl2tpd apt git speedtest-cli p7zip-full libjpeg-dev \
+  zlib1g-dev python3-full shc build-essential nodejs nginx php \
+  php-fpm php-cli php-mysql p7zip-full squid libcurl4-openssl-dev lsb-release 
+apt purge -y apache2 stunnel4 stunnel
+sudo systemctl enable chrony --now
+chronyc sourcestats -v
+chronyc tracking -v
 print_success "Packet Yang Dibutuhkan"
 }
 clear
